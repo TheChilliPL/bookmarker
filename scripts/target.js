4b4045be86913a22819066428d615aa56c1b4fcc
TheChilliPL <dragonacchilli@gmail.com>
2021-04-30 23:05:11 +0200

    Disable auto redirect the first time user uses the bookmark

--- scripts/target.js
+++ scripts/target.js
@@ -14,8 +14,28 @@ faviconElement.rel = "icon";
 faviconElement.href = decode(query.get("icon"));
 document.head.appendChild(faviconElement);
 
-setTimeout(() => {
+function redirect() {
+    localStorage.setItem(window.location.href + " auto", 1);
+
     let url = decode(query.get("url"));
     if(url)
         document.location.href = url;
+}
+
+function shouldAutoRedirect() {
+    let queryAuto = decode(query.get("auto"));
+
+    if(queryAuto && queryAuto !== "1") return false;
+
+    if(localStorage) {
+        let auto = localStorage.getItem(window.location.href + " auto");
+
+        if(auto && auto === "1") return true;
+    }
+
+    return false;
+}
+
+setTimeout(() => {
+    if(shouldAutoRedirect()) redirect();
 }, 0);
